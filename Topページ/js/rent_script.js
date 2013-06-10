892775246cc9bc7d4d215da601f783d05490657c
Masahiro Matsumoto <donden.poka@gmail.com>
2013-06-10 16:09:15 +0900

    物件情報ページ（royal_1, my2_1)のjsファイル作成
    rent_script.jp作成
    on-off画像切り替え

---
+++ Topページ/js/rent_script.js
@@ -0,0 +1,11 @@
+﻿/*ロールオーバー画像切り替え
+**************************************/
+$(function(){
+	$('a img').hover(function(){
+		$(this).attr('src', $(this).attr('src').replace('_off', '_on'));
+	}, function(){
+		if (!$(this).hasClass('currentPage')) {
+			$(this).attr('src', $(this).attr('src').replace('_on', '_off'));
+		}
+	});
+});
